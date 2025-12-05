f37d91b762ecfb164fa4048a59d27591dea6d85c
MalekMbarek <146542756+MalekMbarek@users.noreply.github.com>
2025-12-05 17:29:11 +0100

    not case sensitive

--- HotelClient.java
+++ HotelClient.java
@@ -12,11 +12,11 @@ public class HotelClient{
             Naming.lookup("rmi://localhost:1099/Hotel");
 
         if(args.length >= 1){
-            if(args[0].equals("list")){
+            if(args[0].equalsIgnoreCase("list")){
                 System.out.println(h.list());
-            }else if(args[0].equals("book") && args.length == 3){
+            }else if(args[0].equalsIgnoreCase("book") && args.length == 3){
                 System.out.println(h.book(args[1],args[2]));
-            }else if(args[0].equals("guests")){
+            }else if(args[0].equalsIgnoreCase("guests")){
             List<String> my_guests = new ArrayList<String>();
             my_guests = h.guests();
             if(my_guests.size() == 0){
@@ -27,17 +27,17 @@ public class HotelClient{
                      System.out.println(guest);
                 }
             }
-            }else if(args[0].equals("revenue")){
+            }else if(args[0].equalsIgnoreCase("revenue")){
             System.out.println(h.revenue());
             }
         }else{
            		System.out.println("");
-				System.out.println("Please use anyone of the following commands for RoomManagerImpl:");
+				System.out.println("Please use anyone of the following commands for RoomManagerImpl :");
 				System.out.println("");
 				System.out.println("java HotelClient list");
 				System.out.println("java HotelClient book <room type(Single-Double-Triple)> <guest name>");
-				System.out.println("java HotelClient guests :");
-				System.out.println("java HotelClient revenue :");
+				System.out.println("java HotelClient guests");
+				System.out.println("java HotelClient revenue");
 				System.out.println("");
         }
 
